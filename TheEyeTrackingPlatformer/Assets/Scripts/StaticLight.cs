8b9e0ea2beb6088eccc267d2f097607352d3dc3e
Snorro1998 <tjaard.van.verseveld@gmail.com>
2019-10-09 00:53:00 +0200

    Put several things together

---
+++ TheEyeTrackingPlatformer/Assets/Scripts/StaticLight.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class StaticLight : MonoBehaviour
+{
+    Light2D lamp;
+    public GameObject enemy;
+
+    public float maxDistance = 12;
+    public float minDistance = 4;
+
+    public float distance;
+
+    private IEnumerator blinkCoroutine = null;
+
+    void Start()
+    {
+        lamp = GetComponent<Light2D>();
+    }
+
+    private IEnumerator Blinking()
+    {
+        while (true)
+        {
+            lamp.intensity = 0;
+            yield return new WaitForSeconds(Random.Range(0.01f, 0.1f));
+            lamp.intensity = 1;
+            float waitTime = (distance - minDistance) / 5.0f;
+            yield return new WaitForSeconds(Random.Range(0.1f, 1) * waitTime);
+        }
+    }
+
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, enemy.transform.position);
+        int mode = distance < minDistance ? 2 : distance < maxDistance ? 1 : 0;
+
+        switch (mode)
+        {
+            case 0:
+                lamp.intensity = 1;
+                if (blinkCoroutine != null)
+                {
+                    StopCoroutine(blinkCoroutine);
+                    blinkCoroutine = null;
+                }
+                break;
+            case 1:
+                if (blinkCoroutine == null)
+                {
+                    blinkCoroutine = Blinking();
+                    StartCoroutine(blinkCoroutine);
+                }
+                break;
+            case 2:
+                lamp.intensity = 0;
+                if (blinkCoroutine != null)
+                {
+                    StopCoroutine(blinkCoroutine);
+                    blinkCoroutine = null;
+                }
+                break;
+        }
+    }
+}
---
+++ TheEyeTrackingPlatformer/Assets/Scripts/weightPlatform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weightPlatform : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float masTotal = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //masTotal = rb.mass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector2(0,100));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            masTotal += collision.gameObject.GetComponent<Rigidbody2D>().mass;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            masTotal -= collision.gameObject.GetComponent<Rigidbody2D>().mass;
+        }
+    }
+}
